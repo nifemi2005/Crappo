5b474fa2c96267c5f4018f22c20871b74616fdaa
Temenu Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2026-08-30 13:13:09 +0100

    Remove injected createRequire shim (security cleanup)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
